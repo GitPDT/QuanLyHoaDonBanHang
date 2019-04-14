82a9b431738a93d4fe55d06ca840f428881af3e0
trungdai97 <letrungdai@gmail.com>
2019-04-14 14:27:51 +0700

    Viet UnitTest Ham Tinh Tien

--- UnitTest_HD/TestHoaDon.cs
+++ UnitTest_HD/TestHoaDon.cs
@@ -12,12 +12,28 @@ namespace UnitTest_HD
     [TestClass]
     public class TestHoaDon
     {
-        
+        HoaDon_BUS HoadonBus = new HoaDon_BUS();
         [TestMethod]
         public void TestHamThanhTien()
         {
-            //Test
-            
+            //Tinh tien
+            int dongia = 10000;
+            int soluong = 5;
+            int chietkhau = 10;
+            int actual = HoadonBus.TinhTien(soluong, dongia, chietkhau);
+            int expected = 45000;
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestHamThanhTien_NhapDonGiaBang0()
+        {
+            //Tinh tien
+            int dongia = 0;
+            int soluong = 5;
+            int chietkhau = 10;
+            int actual = HoadonBus.TinhTien(soluong, dongia, chietkhau);
+            int expected = 0;
+            Assert.AreEqual(expected, actual);
         }
     }
 }
